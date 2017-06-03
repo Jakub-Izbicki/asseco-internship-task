3143f1bc9310a6080480c813470b45c055fe18d0
Jakub-Izbicki <jakub.izbicki1@gmail.com>
2017-06-03 20:11:47 +0200

    refactors DownloadCertificateService

--- src/main/java/asseco/intership/task/certificate/download/DownloadCertificateService.java
+++ src/main/java/asseco/intership/task/certificate/download/DownloadCertificateService.java
@@ -3,16 +3,13 @@ package asseco.intership.task.certificate.download;
 import asseco.intership.task.base.AbstractService;
 import asseco.intership.task.certificate.model.Certificate;
 import asseco.intership.task.error.RuntimeErrorController;
-import asseco.intership.task.util.CertFactory;
 import asseco.intership.task.util.FileUtil;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
 import java.io.IOException;
-import java.security.cert.CertificateException;
 
 class DownloadCertificateService extends AbstractService {
 
@@ -30,33 +27,27 @@ class DownloadCertificateService extends AbstractService {
     }
 
     void downloadPem(Certificate certificate) {
-        try {
-            download(certificate, certificate.getRawData().getBytes(), PEM_EXTENSION);
-        } catch (IOException e) {
-            runtimeErrorController.showErrorPopup(
-                    downloadCertificateControllerProvider.get(), "runtimeErrorDownloadCertificate");
-        }
+        download(certificate, certificate.getRawData().getBytes(), PEM_EXTENSION);
     }
 
     void downloadCer(Certificate certificate) {
-        try {
-            byte[] certRawData = CertFactory.of(certificate.getRawData()).getEncoded();
-            download(certificate, certificate.getRawData().getBytes(), CER_EXTENSION);
-        } catch (CertificateException | Base64DecodingException | IOException e) {
-            runtimeErrorController.showErrorPopup(
-                    downloadCertificateControllerProvider.get(), "runtimeErrorDownloadCertificate");
-        }
+        download(certificate, certificate.getRawData().getBytes(), CER_EXTENSION);
     }
 
-    private void download(Certificate certificate, byte[] rawBytes, String extension)
-            throws IOException {
+    private void download(Certificate certificate, byte[] rawBytes, String extension) {
         File selectedDirectory = new DirectoryChooser()
                 .showDialog(downloadCertificateControllerProvider.get().getStage());
         String filename = certificate.getCommonName() + certificate.getId();
-        FileUtil.saveToFile(
-                selectedDirectory.getAbsolutePath(),
-                filename,
-                rawBytes,
-                extension);
+        try {
+            FileUtil.saveToFile(
+                    selectedDirectory.getAbsolutePath(),
+                    filename,
+                    rawBytes,
+                    extension);
+        } catch (IOException e1) {
+            runtimeErrorController.showErrorPopup(
+                    downloadCertificateControllerProvider.get(),
+                    "runtimeErrorDownloadCertificate");
+        }
     }
 }
