c799eb5b404b32e9e0a1472066035fbeec94a8d9
Jakub-Izbicki <jakub.izbicki1@gmail.com>
2017-06-21 17:47:23 +0200

    changes access level for ClientFactory

--- src/main/java/asseco/intership/task/base/ClientFactory.java
+++ src/main/java/asseco/intership/task/base/ClientFactory.java
@@ -4,7 +4,7 @@ import asseco.intership.task.util.PropertiesGetter;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public final class ClientFactory {
+final class ClientFactory {
 
     private static final Retrofit clientBuilder =
             new Retrofit.Builder().baseUrl(new PropertiesGetter().getProperty("api.address"))
@@ -13,7 +13,7 @@ public final class ClientFactory {
     private ClientFactory() {
     }
 
-    public static <T> T of(Class<T> tClass) {
+    static <T> T of(Class<T> tClass) {
         return clientBuilder.create(tClass);
     }
 }
